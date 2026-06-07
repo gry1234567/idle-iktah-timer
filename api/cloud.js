e90aaa07bfd130118c8ac6a34df2d9b382559b10
gry1234567 <power739hot@gmail.com>
2026-06-08 05:10:39 +0800

    Add files via upload

--- api/cloud.js
+++ api/cloud.js
@@ -3,7 +3,7 @@ import https from "https";
 const SUPABASE_REST_URL = "https://oivbidfpeuddedsucwhg.supabase.co/rest/v1/";
 const SUPABASE_ROOT_URL = "https://oivbidfpeuddedsucwhg.supabase.co";
 const PUBLISHABLE_KEY = "sb_publishable_dFemdW0JzlnskIPhMAGTIA_cu-263-G";
-const API_VERSION = "OVERWRITE-UPLOAD-FIX-20260608-06";
+const API_VERSION = "LOCAL-EDIT-ONLY-UPLOAD-SYNC-20260608-07";
 
 function requestJson(path, options = {}) {
   return new Promise((resolve, reject) => {
